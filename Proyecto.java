a13e0458a4a327a27bc1b2f8b671705c45107b33
Nixuna <alfredojoubert98@fpuna.edu.py>
2021-01-15 13:14:12 -0300

    Inicio del proyecto

---
+++ Proyecto.java
@@ -0,0 +1,11 @@
+//Este es un programa que variara dentro de los dias 
+//No es un proyecto en si, si no una base para el aprendizaje del lenguaje java
+
+public class Proyecto
+{
+	public static void main (String args[])
+	{
+		System.out.println("Hola Mundo!!");
+	}
+}
+
