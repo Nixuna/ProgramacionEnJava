055c7739a538be5dad8f8b4023f76fa81b6d12a4
Nixuna <alfredojoubert98@fpuna.edu.py>
2021-01-15 20:02:26 -0300

    Creacion de la clase principal

---
+++ CenterLibroCalificaciones.java
@@ -0,0 +1,13 @@
+//Se estara empezando a usar objetos y metodos
+//Este programa sera un ejemplo de ello, en el trascurso de los dias este programa variara
+
+public class CenterLibroCalificaciones
+{
+	public static void main (String args[])
+	{
+		LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+
+		miLibroCalificaciones.mostrarMensajes();
+	}
+
+}
