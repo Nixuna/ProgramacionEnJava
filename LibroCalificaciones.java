8b29d604485a1516285e008f461d879f9cdd0340
Nixuna <alfredojoubert98@fpuna.edu.py>
2021-01-15 20:03:50 -0300

    Clase donde se encuentra el metodo mostrarMensajes

---
+++ LibroCalificaciones.java
@@ -0,0 +1,10 @@
+//Declaracion de una clase con un metodo
+
+public class LibroCalificaciones
+{
+   public void mostrarMensajes()
+   {
+      System.out.println("Bienvenido a LibroCalificaciones");
+   }
+}
+
