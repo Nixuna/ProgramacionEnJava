883b1a4cdc326d60616601f8b62c255553eb06d2
Nixuna <alfredojoubert98@fpuna.edu.py>
2021-01-18 17:25:30 -0300

    Agregado de 2 funciones mas

--- LibroCalificaciones.java
+++ LibroCalificaciones.java
@@ -2,9 +2,21 @@
 
 public class LibroCalificaciones
 {
+	private String nombreDelCurso; //Variable de instancia
+
+	public void establecerNombreDelCurso(String nombre)
+	{
+		nombreDelCurso = nombre;
+	}
+
+	public String obtenerNombreDelCurso()
+	{
+		return nombreDelCurso;
+	}
+
    public void mostrarMensajes(String nombreDelCurso)
    {
-      System.out.printf("Bienvenido a LibroCalificaciones\n%s\n", nombreDelCurso);
+      System.out.printf("Bienvenido a LibroCalificaciones\n%s\n", obtenerNombreDelCurso());
    }
 }
 
